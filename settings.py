08370174f572bebb7fe195ca76208e1686c6ca17
MarshallLeeKing <ykapitonov@.gmail.com>
2022-10-19 02:04:38 +0300

    Начальная настройка. Создание и отрисовка окна.

---
+++ settings.py
@@ -0,0 +1,10 @@
+
+# Настройка игры
+class Settings:
+
+    def __init__(self) -> None:
+
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
